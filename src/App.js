1aa575bbcc59f9e1223b66c30143f0657e134530
Carq <lciura.pl@gmail.com>
2020-01-05 17:06:54 +0100

    Fixed issue with ThemeProvide

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ThemeProvider } from "@material-ui/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 import Dashboard from "./Dashboard";
 import theme from "./Theme/theme";
 import "./App.css";
