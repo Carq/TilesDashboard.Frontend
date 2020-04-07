223997bc25cf90e02e8a94465fb5908c289e400f
Carq <lciura.pl@gmail.com>
2020-04-07 10:40:11 +0200

    Updates

---
+++ precache-manifest.42e13858e3ffeb6ab398e6afcb8e7db6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe54b397cce5174a7f7fed9dd859dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "168a8852e10c0122f06c",
+    "url": "/static/css/main.2b934300.chunk.css"
+  },
+  {
+    "revision": "7fafddac61ca1f3a49ac",
+    "url": "/static/js/2.2b164b50.chunk.js"
+  },
+  {
+    "revision": "cd3d1e1913e6fef0da5deaa4b79eca6e",
+    "url": "/static/js/2.2b164b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "168a8852e10c0122f06c",
+    "url": "/static/js/main.fa5dbe9a.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
